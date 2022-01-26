7e8f81c2c299fb2cac16919d84a0d519437b865e
Axel Olausson Holtenäs <vermium@zyner.org>
2022-01-26 21:53:46 +0100

    Fixed wrong path for command

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -4,11 +4,16 @@ const { Routes } = require('discord-api-types/v9');
 const { clientId, guildId, zynerId, token } = require('./config.json');
 
 const commands = [];
-const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith('.js'));
+const commandFolders = fs.readdirSync('./src/commands');
 
-for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
-  commands.push(command.data.toJSON());
+for (const folder of commandFolders) {
+  const commandFiles = fs
+    .readdirSync(`./src/commands/${folder}`)
+    .filter((file) => file.endsWith('.js'));
+  for (const file of commandFiles) {
+    const command = require(`./commands/${folder}/${file}`);
+    commands.push(command.data.toJSON());
+  }
 }
 
 const rest = new REST({ version: '9' }).setToken(token);
