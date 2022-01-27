f743af3b1acf37196d15290183dc76ba8124426a
Axel Olausson Holtenäs <vermium@zyner.org>
2022-01-27 15:13:34 +0100

    🔊 error.js

--- src/handlers/error.js
+++ src/handlers/error.js
@@ -1,5 +1,21 @@
 const { MessageEmbed } = require('discord.js');
-
+const error = require('../db/models/errorSchema');
 module.exports = async (interaction, embed, ephemeral) => {
-  interaction.reply({ embeds: [embed], ephemeral });
+  const { user } = interaction;
+
+  const interactionJSON = interaction.toJSON();
+  await error
+    .create({ command: interaction, interaction: interactionJSON })
+    .then(async (response) => {
+      const template = {
+        color: 0xf76363,
+        footer: {
+          text: `${user.username}︱ ID: ${response._id}`,
+          icon_url: user.displayAvatarURL(),
+        },
+      };
+      embed = { ...embed, ...template };
+
+      interaction.reply({ embeds: [embed], ephemeral: embed.ephemeral || false });
+    });
 };
