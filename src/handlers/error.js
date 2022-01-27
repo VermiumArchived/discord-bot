8bd31d053d96fdaf0bc1f5e97136f85752f6941a
Axel Olausson Holtenäs <vermium@zyner.org>
2022-01-27 15:31:35 +0100

    handlers/error: add optional footer

--- src/handlers/error.js
+++ src/handlers/error.js
@@ -10,7 +10,7 @@ module.exports = async (interaction, embed, ephemeral) => {
       const template = {
         color: 0xf76363,
         footer: {
-          text: `${user.username}︱ ID: ${response._id}`,
+          text: `${user.username}︱ ID: ${response._id}${embed.footer}`,
           icon_url: user.displayAvatarURL(),
         },
       };
