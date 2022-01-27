e18f83619a2089b4c814cb2bec3c1a4801089e2b
Axel Olausson Holtenäs <vermium@zyner.org>
2022-01-27 15:13:48 +0100

    🔊 success

--- src/handlers/success.js
+++ src/handlers/success.js
@@ -1,5 +1,20 @@
 const { MessageEmbed } = require('discord.js');
+const success = require('../db/models/successSchema');
 
 module.exports = async (interaction, embed, ephemeral) => {
-  interaction.reply({ embeds: [embed], ephemeral });
+  const { user } = interaction;
+  const interactionJSON = interaction.toJSON();
+  await success
+    .create({ command: interaction, interaction: interactionJSON })
+    .then(async (response) => {
+      const template = {
+        color: 0x99cc99,
+        footer: {
+          text: `${user.username}︱ ID: ${response._id}`,
+          icon_url: user.displayAvatarURL(),
+        },
+      };
+      embed = { ...embed, ...template };
+      interaction.reply({ embeds: [embed], ephemeral: embed.ephemeral || false });
+    });
 };
