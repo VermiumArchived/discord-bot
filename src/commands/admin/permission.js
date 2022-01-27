62ceee90a2fde5af45cae7b4102dd370cbfa5dd9
Axel Olausson Holtenäs <vermium@zyner.org>
2022-01-27 12:56:11 +0100

    Added list, remove to permission system

--- src/commands/admin/permission.js
+++ src/commands/admin/permission.js
@@ -23,6 +23,25 @@ module.exports = {
         )
     )
     .addSubcommand((subcommand) =>
+      subcommand
+        .setName('remove')
+        .setDescription('Remove permission')
+        .addRoleOption((option) =>
+          option.setName('role').setDescription('RoleID').setRequired(true)
+        )
+        .addStringOption((option) =>
+          option.setName('command').setDescription('Command').setRequired(true)
+        )
+    )
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName('list')
+        .setDescription('List role permission')
+        .addRoleOption((option) =>
+          option.setName('role').setDescription('RoleID').setRequired(true)
+        )
+    )
+    .addSubcommand((subcommand) =>
       subcommand.setName('default').setDescription('Add default permission')
     ),
   async execute(interaction) {
@@ -58,6 +77,86 @@ module.exports = {
             //   thumbnail: true,
             // });
           });
+      }
+    } else if (interaction.options.getSubcommand() === 'default' && interaction.inGuild() == true) {
+      const { guild } = interaction;
+      let perms = [];
+      const commandFolders = fs.readdirSync('./src/commands');
+
+      for (const folder of commandFolders) {
+        const commandFiles = fs
+          .readdirSync(`./src/commands/${folder}`)
+          .filter((file) => file.endsWith('.js'));
+        for (const file of commandFiles) {
+          const command = require(`../../commands/${folder}/${file}`);
+          const commandData = command.data.toJSON();
+          if (command.permission.default === true) {
+            perms.push(commandData.name);
+          }
+        }
+      }
+      const defaultPermDB = await permission.findOne({ guildID: guild.id, roleID: guild.id });
+      if (!defaultPermDB) {
+        console.log('Not Found');
+        permission
+          .create({ guildID: guild.id, roleID: guild.id, permissions: perms })
+          .then(async () => {
+            console.log('Created default', {
+              guildID: guild.id,
+              roleID: guild.id,
+              permissions: perms,
+            });
+            // return successHandler(interaction, {
+            //   title: 'Default Permission',
+            //   description: 'Successfully created default permissions',
+            //   ephemeral: true,
+            //   thumbnail: true,
+            // });
+          });
+      }
+    } else if (interaction.options.getSubcommand() === 'remove' && interaction.inGuild() == true) {
+      const role = interaction.options.getRole('role');
+      const command = interaction.options.getString('command');
+      const guild = interaction.guild;
+      let permissions = [];
+      permissions.push(command);
+      const roleDB = await permission.findOne({ guildID: guild.id, roleID: role.id });
+      console.log(permissions);
+      console.log(roleDB);
+      if (roleDB) {
+        console.log(roleDB);
+        await permission
+          .findOneAndUpdate(
+            { guildID: guild.id, roleID: role.id },
+            { $pull: { permissions: command } }
+          )
+          .then(async () => {
+            console.log('Removed Permission', { guildID: guild.id, roleID: role.id });
+            return success(interaction, {
+              title: 'Removed Permission',
+              description: `Successfully removed permission to execute command: ${command} for ${role}.`,
+              ephemeral: true,
+              thumbnail: true,
+            });
+          });
+      } else {
+        console.log('NOT FOUND');
+      }
+    } else if (interaction.options.getSubcommand() === 'list' && interaction.inGuild() == true) {
+      const role = interaction.options.getRole('role');
+      const guild = interaction.guild;
+      const roleDB = await permission.findOne({ guildID: guild.id, roleID: role.id });
+      console.log(roleDB);
+      if (roleDB) {
+        console.log(roleDB);
+        return success(interaction, {
+          title: 'Removed Permission',
+          description: `${role} has permission to execute:\n${roleDB.permissions.map((x) => x)}`,
+          ephemeral: true,
+          thumbnail: true,
+        });
+      } else {
+        console.log('NOT FOUND');
       }
     } else if (interaction.options.getSubcommand() === 'default' && interaction.inGuild() == true) {
       const { guild } = interaction;
