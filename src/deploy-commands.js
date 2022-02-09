cb9ce335b30b487babe519a15f85bf8546afab58
Axel Olausson Holtenäs <vermium@zyner.org>
2022-02-09 19:53:38 +0100

    🎨 organized src/deploy-commands.js

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,34 +1,27 @@
 const fs = require('fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { clientId, guildId, zynerId, token } = require('./config.json');
-const permission = require('./db/models/permissionSchema');
+const { clientId, guildId, token } = require('./config.json');
+const logger = require('./logger');
+
 const commands = [];
 
-const commandCategories = fs.readdirSync('./src/commands');
-
-for (const category of commandCategories) {
-  const cmds = fs.readdirSync(`./src/commands/${category}`);
-
-  for (const command of cmds) {
-    const cmd = require(`./commands/${category}/${command}/index.js`);
+fs.promises.readdir('./src/commands').then(async (category) => {
+  fs.promises.readdir(`./src/commands/${category}`).then((command) => {
+    /* eslint-disable global-require, import/no-dynamic-require */
+    const cmd = require(`./commands/${category}/${command}`);
     commands.push(cmd.data.toJSON());
-  }
-}
+  });
+});
 
 const rest = new REST({ version: '9' }).setToken(token);
 
 rest
   .put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-  .then(() => console.log('Successfully registered guild application commands.'))
-  .catch(console.error);
-
-rest
-  .put(Routes.applicationGuildCommands(clientId, zynerId), { body: commands })
-  .then(() => console.log('Successfully registered guild application commands.'))
-  .catch(console.error);
+  .then(() => logger.info('Successfully registered guild application commands.'))
+  .catch(logger.error);
 
 rest
   .put(Routes.applicationCommands(clientId), { body: commands })
-  .then(() => console.log('Successfully registered application commands.'))
-  .catch(console.error);
+  .then(() => logger.info('Successfully registered application commands.'))
+  .catch(logger.error);
