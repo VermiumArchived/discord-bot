6fb41b689de1e1206cb79a94458036d97eb7b6b7
Axel Olausson Holtenäs <vermium@zyner.org>
2022-02-09 19:57:05 +0100

    🔊 added logger component to src/db/index.js

--- src/db/index.js
+++ src/db/index.js
@@ -1,15 +1,17 @@
 const mongoose = require('mongoose');
 
 const { mongodb } = require('../config.json');
+
 const { connectionString } = mongodb;
+const logger = require('../logger');
 
 module.exports = {
-  async connectDB() {
+  async connect() {
     try {
       await mongoose.connect(connectionString);
-      console.log('Connected to the database');
+      logger.info('Connected to the database');
     } catch (e) {
-      console.log(e);
+      logger.error(e);
     }
   },
 };
