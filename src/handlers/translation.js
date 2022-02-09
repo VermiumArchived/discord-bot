27514df28636877dcf4296578ccca587b29a0e42
Axel Olausson Holtenäs <vermium@zyner.org>
2022-02-09 21:27:11 +0100

    🌐 added some translations

--- src/handlers/translation.js
+++ src/handlers/translation.js
@@ -1,13 +1,29 @@
 const i18next = require('i18next');
 
-module.exports = () => {
-  i18next.init({
+module.exports = async () => {
+  await i18next.init({
     lng: 'en', // if you're using a language detector, do not define the lng option
     debug: true,
+    fallbackLng: 'en',
     resources: {
       en: {
-        common: {
-          ready: 'Ready! Logged in as {{client.user.tag}}!',
+        ready: { message: 'Ready! Logged in as {{client.user.tag}}!' },
+        db: {
+          connected: 'Connected to the database!',
+        },
+        deployCommands: {
+          global: 'Successfully registered application commands.',
+          guild: 'Successfully registered guild application commands.',
+        },
+      },
+      sv: {
+        ready: { message: 'Redo! Inloggad som {{client.user.tag}}!' },
+        db: {
+          connected: 'Ansluten till databasen!',
+        },
+        deployCommands: {
+          global: 'Registrerade programkommandon.',
+          guild: 'Registrerade guild-applikationskommandon.',
         },
       },
     },
